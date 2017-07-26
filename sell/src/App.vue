138dfe002813b38348c687eab9fc0e72eb694ab8
Joansnail <850337486@qq.com>
2017-07-26 19:47:09 +0800

    test

---
+++ sell/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1 v-text="title"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+ data: function(){
+  return {
+    title : 'this is a title'
+  }
+ }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
